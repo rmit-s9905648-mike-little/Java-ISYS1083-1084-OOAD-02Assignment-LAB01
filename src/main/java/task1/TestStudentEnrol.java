8906155d6e460cd1b9ec45256ee24d4814526dc7
FlashJackFromGungagai <hypercomms2001@gmail.com>
2024-05-14 20:19:44 +1000

    This represents the changes to Task 01 as of Tuesday the 14th of May 2024

---
+++ src/main/java/task1/TestStudentEnrol.java
@@ -0,0 +1,16 @@
+package task1;
+
+public class TestStudentEnrol
+{
+
+    public static void main(String[] args)
+    {
+        StudentEnrol se = new StudentEnrol();
+        //
+        se.addCore("P1");
+        se.removeCore("P1");
+        se.removeCore("P1");
+        se.display();
+    }
+
+}// close public class TestStudentEnrol
